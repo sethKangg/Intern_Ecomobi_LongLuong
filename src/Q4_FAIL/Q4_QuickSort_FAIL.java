b254c552f2003e505dc1279ff06e0daf72972c3f
sethKangg <91963611+sethKangg@users.noreply.github.com>
2021-12-27 22:34:01 +0700

    Project Intern Ecomobi

---
+++ src/Q4_FAIL/Q4_QuickSort_FAIL.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Q4_FAIL;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Q4_QuickSort_FAIL {
+
+    /**
+     * @param args the command line arguments
+     */
+   public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number of array:");
+        int length = scanner.nextInt();
+        int[] array = new int[length];
+        for (int i = 0; i < length; i++) {
+            array[i] = new Random().nextInt(length);
+        }
+        Q4_QuickSort_FAIL sorter = new Q4_QuickSort_FAIL();
+        
+        System.out.print("Unsorted array: ");
+        sorter.displayArray(array);
+        
+        System.out.println();
+        
+        sorter.quickSort(array, 0, length - 1);
+        System.out.print("Sorted array: ");
+        sorter.displayArray(array);
+    }
+
+    /**
+     *
+     * @param arr the array
+     */
+    public void displayArray(int[] arr) {
+        System.out.print("[");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+            if (i < arr.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.print("]");
+    }
+
+    int partition(int arr[], int left, int right) {
+        int i = left, j = right;
+        int tmp;
+        int pivot = arr[(left + right) / 2];
+
+        while (i <= j) {
+            while (arr[i] < pivot) {
+                i++;
+            }
+            while (arr[j] > pivot) {
+                j--;
+            }
+            if (i <= j) {
+                tmp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = tmp;
+                i++;
+                j--;
+            }
+        };
+        return i;
+    }
+
+    void quickSort(int arr[], int left, int right) {
+        int index = partition(arr, left, right);
+        if (left < index - 1) {
+            quickSort(arr, left, index - 1);
+        }
+        if (index < right) {
+            quickSort(arr, index, right);
+        }
+    }
+    
+}
